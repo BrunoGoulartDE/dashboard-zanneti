770ed32224eff46eb82d76e264f0480689daba8f
Bruno Oliveira Goulart <bruno.goulart1414@gmail.com>
2024-10-22 21:24:34 -0400

    Feat: Redirecionamento do SideBar

--- dashboard-zanetti/src/components/Sidebar/index.tsx
+++ dashboard-zanetti/src/components/Sidebar/index.tsx
@@ -3,13 +3,14 @@ import { BiHome, BiStore, BiUserPlus } from "react-icons/bi";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Button } from "../Button";
+import { CgGym } from "react-icons/cg";
 
 export function Sidebar() {
   const location = useLocation();
   const [isOpenSidebar, setIsOpenSidebar] = useState(false);
+  const [isActiveExercises, setIsActiveExercises] = useState(false);
+  const [isActiveWorkouts, setIsActiveWorkouts] = useState(false);
   const [isActiveStudents, setIsActiveStudents] = useState(false);
-  const [isActiveAcademy, setIsActiveAcademy] = useState(false);
-
   return (
     <div className="h=[calc(100vh-73px)]">
       <aside
@@ -37,29 +38,29 @@ export function Sidebar() {
           <li
             className="group overflow-hidden cursor-pointer hover:bg-[#B984DB] data-[selected=true]:bg-[#ffffff]"
             data-active={isActiveStudents}
-            data-selected={location.pathname.includes("/academy")}
+            data-selected={location.pathname.includes("/Workouts")}
           >
             <button
               className="py-2.5 flex items-center justify-between text-black w-full hover:bg-[#B984DB] bg-[#ffffff]"
-              onClick={() => setIsActiveAcademy(!isActiveAcademy)}
+              onClick={() => setIsActiveWorkouts(!isActiveWorkouts)}
             >
               <a
-                href="/academy"
+                href="/treinos"
                 className="flex items-center"
-                data-selected={location.pathname.includes("academy/classes")}
+                data-selected={location.pathname.includes("treinos")}
               >
                 <BiStore className="min-w-[33px] h-4 text-center" />
                 <span
                   className="font-semibold transition-all text-sm delay-100 opacity-0 data-[close=false]:block data-[close=false]:!opacity-100"
                   data-close={!isOpenSidebar}
                 >
-                  Academia
+                  Treinos
                 </span>
               </a>
               <ChevronDown
                 className="min-w-[48px] h-6 transition-all duration-300 opacity-0 data-[close=false]:block data-[close=false]:!opacity-100 data-[active=true]:rotate-[180deg]"
                 data-close={!isOpenSidebar}
-                data-active={!isActiveAcademy}
+                data-active={!isActiveWorkouts}
               />
             </button>
 
@@ -67,41 +68,34 @@ export function Sidebar() {
             <ul
               className="hidden data-[active=true]:block pb-1 pr-2 pl-16  bg-[#ffffff] data-[close=true]:absolute data-[close=true]:z-50 data-[close=true]:left-[100%] data-[close=true]:!-mt-14 data-[close=true]:py-2 data-[close=true]:px-5 data-[close=true]:min-w-max data-[close=true]:rounded-r-md data-[close=true]:hidden data-[close=true]:opacity-0 data-[close=true]:group-hover:block data-[close=true]:group-hover:opacity-100"
               data-close={!isOpenSidebar}
-              data-active={!isActiveAcademy}
+              data-active={!isActiveWorkouts}
             >
               <li>
                 <h3
                   className="text-black text-lg mt-2 mb-1 font-semibold hidden data-[close=true]:block"
                   data-close={!isOpenSidebar}
                 >
-                  Academia
+                  Treinos
                 </h3>
               </li>
               <li>
                 <a
-                  href="/academy/classes"
+                  href="/treinos/adicionar"
                   className="text-black text-sm py-3 block whitespace-nowrap opacity-60 transition-opacity hover:opacity-100 data-[selected=true]:opacity-100"
-                  data-selected={location.pathname.includes("academy/classes")}
+                  data-selected={location.pathname.includes("treino/adicionar")}
                 >
-                  Turmas
+                  Adicionar treino
                 </a>
               </li>
               <li>
                 <a
-                  href="/profile/plans"
+                  href="/treinos/visualizar"
                   className="text-black text-sm py-3 block whitespace-nowrap opacity-60 transition-opacity hover:opacity-100 data-[selected=true]:opacity-100"
-                  data-selected={location.pathname.includes("profile/plans")}
+                  data-selected={location.pathname.includes(
+                    "treinos/visualizar"
+                  )}
                 >
-                  Planos
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/profile/gym"
-                  className="text-black text-sm py-3 block whitespace-nowrap opacity-60 transition-opacity hover:opacity-100 data-[selected=true]:opacity-100"
-                  data-selected={location.pathname.includes("/profile/gym")}
-                >
-                  Perfil
+                  Visualizar treinos
                 </a>
               </li>
             </ul>
@@ -111,13 +105,13 @@ export function Sidebar() {
           <li
             className="group overflow-hidden cursor-pointer hover:bg-[#ffffff] data-[selected=true]:bg-[#ffffff]"
             data-active={isActiveStudents}
-            data-selected={location.pathname.includes("/students")}
+            data-selected={location.pathname.includes("/alunos")}
           >
             <button
               className="py-2.5 flex items-center justify-between text-black w-full hover:bg-[#B984DB] bg-[#ffffff]"
               onClick={() => setIsActiveStudents(!isActiveStudents)}
             >
-              <a href="/students" className="flex items-center">
+              <a href="/alunos" className="flex items-center">
                 <BiUserPlus className="min-w-[35px] h-4" />
                 <span
                   className="font-semibold transition-all text-sm delay-100 opacity-0 data-[close=false]:block data-[close=false]:!opacity-100"
@@ -149,11 +143,65 @@ export function Sidebar() {
               </li>
               <li>
                 <a
-                  href="/students"
+                  href="/alunos"
                   className="text-black text-sm py-3 block whitespace-nowrap opacity-60 transition-opacity hover:opacity-100 data-[selected=true]:opacity-100"
-                  data-selected={location.pathname.includes("/students")}
+                  data-selected={location.pathname.includes(
+                    "/alunos/gerenciar"
+                  )}
+                >
+                  Gerenciar alunos
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li
+            className="group overflow-hidden cursor-pointer hover:bg-[#ffffff] data-[selected=true]:bg-[#ffffff]"
+            data-active={isActiveExercises}
+            data-selected={location.pathname.includes("/exercicios")}
+          >
+            <button
+              className="py-2.5 flex items-center justify-between text-black w-full hover:bg-[#B984DB] bg-[#ffffff]"
+              onClick={() => setIsActiveExercises(!isActiveExercises)}
+            >
+              <a href="/exercicios" className="flex items-center">
+                <CgGym className="min-w-[35px] h-4" />
+                <span
+                  className="font-semibold transition-all text-sm delay-100 opacity-0 data-[close=false]:block data-[close=false]:!opacity-100"
+                  data-close={!isOpenSidebar}
                 >
                   Alunos
+                </span>
+              </a>
+              <ChevronDown
+                className="min-w-[48px] h-6 transition-all duration-300 opacity-0 data-[close=false]:block data-[close=false]:!opacity-100 data-[active=true]:rotate-[180deg]"
+                data-close={!isOpenSidebar}
+                data-active={isActiveExercises}
+              />
+            </button>
+
+            {/* submenu */}
+            <ul
+              className="hidden data-[active=true]:block pb-1 pr-2 pl-16 bg-[#ffffff] data-[close=true]:absolute data-[close=true]:z-50 data-[close=true]:left-[100%] data-[close=true]:!-mt-14 data-[close=true]:py-2 data-[close=true]:px-5 data-[close=true]:min-w-max data-[close=true]:rounded-r-md data-[close=true]:hidden data-[close=true]:opacity-0 data-[close=true]:group-hover:block data-[close=true]:group-hover:opacity-100"
+              data-close={!isOpenSidebar}
+              data-active={isActiveExercises}
+            >
+              <li className="">
+                <h3
+                  className="text-black text-lg mt-2 mb-1 font-semibold hidden data-[close=true]:block"
+                  data-close={!isOpenSidebar}
+                >
+                  Exercícios
+                </h3>
+              </li>
+              <li>
+                <a
+                  href="/exercicio/adicionar"
+                  className="text-black text-sm py-3 block whitespace-nowrap opacity-60 transition-opacity hover:opacity-100 data-[selected=true]:opacity-100"
+                  data-selected={location.pathname.includes(
+                    "exercicio/adicionar"
+                  )}
+                >
+                  Adicionar exercício
                 </a>
               </li>
             </ul>
@@ -168,7 +216,7 @@ export function Sidebar() {
         }`}
         onClick={() => {
           setIsOpenSidebar(!isOpenSidebar);
-          setIsActiveStudents(false);
+          setIsActiveExercises(false);
         }}
       />
     </div>
