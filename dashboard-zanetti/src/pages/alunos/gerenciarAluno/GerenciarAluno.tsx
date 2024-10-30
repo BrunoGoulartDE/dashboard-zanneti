6156ef2522db571e67fc7bdfb2e0653131b06d01
Bruno Oliveira Goulart <bruno.goulart1414@gmail.com>
2024-10-30 01:12:37 -0400

    Feat: Listagem de alunos

--- dashboard-zanetti/src/pages/alunos/gerenciarAluno/GerenciarAluno.tsx
+++ dashboard-zanetti/src/pages/alunos/gerenciarAluno/GerenciarAluno.tsx
@@ -1,8 +1,34 @@
 import MainLayout from "@/layouts/MainLayout";
-
+import { columns, Aluno } from "./components/columns";
+import { DataTable } from "./components/data-table";
 import InfoBar from "@/layouts/InfoBar";
+import api from "@/api";
+import { useEffect, useState } from "react";
 
 const GerenciarAluno = () => {
+  const [alunos, setAlunos] = useState<Aluno[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const obterAlunos = async () => {
+    setLoading(true);
+    try {
+      const response = await api.get(`/aluno`);
+      console.log(response.data);
+      setAlunos(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Erro ao obter alunos:", error);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    obterAlunos();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <MainLayout>
       <div>
@@ -11,6 +37,9 @@ const GerenciarAluno = () => {
             pageTitle="Gerenciar alunos"
             pageDescription={`Gerencie seus alunos`}
           ></InfoBar>
+          <div className="container mx-auto py-10">
+            <DataTable columns={columns} data={alunos} />
+          </div>
         </div>
       </div>
     </MainLayout>
---
+++ dashboard-zanetti/src/pages/alunos/gerenciarAluno/components/columns.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { BiEdit, BiGlasses, BiTrash } from "react-icons/bi";
+
+export type Aluno = {
+  AlunoID: number;
+  Nome: string;
+  PesoAtual: string;
+  Idade: string;
+  Altura: string;
+  MusculoAlvo: string;
+  Objetivo: string;
+  FrequenciaSemanal: string;
+  Lesoes: string;
+  TempoTreinando: string;
+};
+
+export const columns: ColumnDef<Aluno>[] = [
+  {
+    accessorKey: "Nome",
+    header: "Nome",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const aluno = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Abrir menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Ações para {aluno.Nome}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <BiTrash />
+              Excluir
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <BiEdit />
+              Editar
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
