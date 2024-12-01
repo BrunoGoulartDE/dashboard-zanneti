960e27f5038ba4a4bd25e22ae08607316e0c1d99
Bruno Oliveira Goulart <bruno.goulart1414@gmail.com>
2024-12-01 10:40:19 -0400

    chore

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        outDir: "dist",
+    },
+    preview: {
+        port: parseInt(process.env.PORT || "4173"),
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
